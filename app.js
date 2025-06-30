bb212f34388c76754bd83d27bc2add0a910701d4
Georgia <georgialloydphoto@gmail.com>
2025-06-30 17:33:00 -0500

    Update

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ mongoose
     console.log("Connected to DB");
   })
   .catch((e) => console.error(e));
-
+//
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+//
 app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,7 @@
-// const { DEFAULT_ERROR_STATUS_CODE } = require("../utils/errors/errors");
 const { DefaultError } = require("../utils/errors/defaultError");
 
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-  // return res.status(err.status).send({ message: err.message });
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   } else {
