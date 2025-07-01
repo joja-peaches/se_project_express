6f0bba3c9d3b8b22562d5f4eca47192676e143ae
Georgia <georgialloydphoto@gmail.com>
2025-07-01 15:14:03 -0500

    Update error-handler and validation

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,8 +2,9 @@ const { DefaultError } = require("../utils/errors/defaultError");
 
 const errorHandler = (err, req, res, next) => {
   if (err.status) {
-    next(new DefaultError("An error has occurred on the server."));
+    return res.status(err.status).send({ message: err.message });
   }
+  return next(new DefaultError("An error has occurred on the server."));
 };
 
 module.exports = { errorHandler };
